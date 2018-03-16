fe3a704544f74438ef419c760176c20a47a7926e
root <doctor.kyle.hart@gmail.com>
2018-03-15 21:16:49 -1000

    command line directives and usage statement

--- point_plots.py
+++ point_plots.py
@@ -4,7 +4,18 @@ import sys
 from svm_sim import generate_gaussian_labeled_points, generate_labeled_points, generate_island_labeled_points
 
 if __name__ == "__main__":
+    usage = "\nusage: python3 point_plots.py [distribution] [binary_status] [margin]\n\n" \
+            "\tdistribution: \"linear\", \"gauss\", \"islands\"\n" \
+            "\tbinary_status: \"binary\", \"multi\"(only ok for gaussian)\n" \
+            "\tmargin: float value (-1, 1)"
+    if len(sys.argv) != 4:
+        print(usage)
+        exit(1)
     type = sys.argv[1]
+    if type not in (["linear", "islands", "gauss"]):
+        print(usage)
+        exit(1)
+
     margin = float(sys.argv[3])
     binary = True if sys.argv[2] == "binary" else False
     if type == 'linear':
@@ -14,6 +25,7 @@ if __name__ == "__main__":
     elif type == 'gauss':
         l_points = generate_gaussian_labeled_points(n=1000, n_means=4, dim=2, gamma=margin, binary=binary)
     else:
+        print(usage)
         exit(1)
     points = [x[0] for x in l_points]
     labels = [x[1] for x in l_points]
