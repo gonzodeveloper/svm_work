cdbbb00b4208c09edbd41137431d55666517e3b1
Sayera Dhaubhadel <sayeradh@hawaii.edu>
2018-04-06 08:39:42 -1000

    plotting file added

--- sayera_plot.py
+++ sayera_plot.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-file = "svc_rbf_lin_c_gamma_500runs.csv"
+'''
+file = "Nu_RBF_Nonlinear_1.csv"
 zvar = 'test_error'
 df = pd.read_csv("data/{}".format(file))
 
-max_df = df.loc[df['machine_param'] == 1.9]
+#non_failures = df.loc[df[zvar] >= 0]
+max_df = non_failures.loc[df['machine_param'] == 1.9]
 mean_df = max_df.groupby(['point_param'], as_index=False)['test_error'].mean()
 sd_df = max_df.groupby(['point_param'], as_index=False)['test_error'].std()
 
@@ -20,15 +21,17 @@ sd = sd_df.as_matrix(['test_error'])
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
 ax.scatter(xx, yy)
+ax.plot(xx, yy)
 
 for i in np.arange(0, len(sd)):
     ax.scatter([xx[i], xx[i]], [yy[i] + sd[i], yy[i] - sd[i]],
                marker="_", c='y')
-plt.title("SVC LINEAR KERNEL LINEAR POINTS")
+plt.title("Test Error vs Gamma for NU-SVM RBF KERNEL NON-LINEAR POINTS (nu=0.02)")
 
 ax.set_xlabel("Gamma Value")
 ax.set_ylabel("Test Error")
 plt.show()
+'''
 '''
 ########################################################################3
 
@@ -73,3 +76,64 @@ plt.title(title)
 ax.scatter(xx, yy, zz)
 
 '''
+'''
+file = "gpc_rbf_lin_gamma_01.csv"
+#zvar = 'test_error'
+df = pd.read_csv("data/{}".format(file))
+
+#max_df = df.loc[df['machine_param'] == 1.9]
+mean_df = df.groupby(['point_param'], as_index=False)['test_error'].mean()
+sd_df = df.groupby(['point_param'], as_index=False)['test_error'].std()
+
+
+xx = mean_df.as_matrix(['point_param'])
+yy = mean_df.as_matrix(['test_error'])
+sd = sd_df.as_matrix(['test_error'])
+
+
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
+ax.scatter(xx, yy)
+ax.plot(xx, yy)
+
+for i in np.arange(0, len(sd)):
+    ax.scatter([xx[i], xx[i]], [yy[i] + sd[i], yy[i] - sd[i]],
+               marker="_", c='y')
+plt.title("Test Error vs Gamma for GPC RBF KERNEL LINEAR POINTS")
+
+ax.set_xlabel("Gamma Value")
+ax.set_ylabel("Test Error")
+plt.show()
+'''
+
+file = "Nu_RBF_Nonlinear_1.csv"
+zvar = 'test_error'
+df = pd.read_csv("data/{}".format(file))
+
+non_failures = df.loc[df[zvar] >= 0]
+print(len(df[zvar] >= 0))
+max_df = non_failures.loc[df['point_param'] == 0.6]
+print(max_df)
+mean_df = max_df.groupby(['machine_param'], as_index=False)['test_error'].mean()
+print(mean_df)
+sd_df = max_df.groupby(['machine_param'], as_index=False)['test_error'].std()
+
+
+xx = mean_df.as_matrix(['machine_param'])
+yy = mean_df.as_matrix(['test_error'])
+sd = sd_df.as_matrix(['test_error'])
+
+
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
+ax.scatter(xx, yy)
+ax.plot(xx, yy)
+
+for i in np.arange(0, len(sd)):
+    ax.scatter([xx[i], xx[i]], [yy[i] + sd[i], yy[i] - sd[i]],
+               marker="_", c='y')
+plt.title("NU-SVM RBF KERNEL NON-LINEAR POINTS (point_param=0.6))")
+
+ax.set_xlabel("Nu Value")
+ax.set_ylabel("Test Error")
+plt.show()
